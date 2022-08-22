cf4af0e287b9131683733a62ee61f4e3dc5340eb
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-22 17:43:15 -0400

    home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+function HomePage(){
+    return (
+        <div>
+            <h1>Hello, welcome to my blog!</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. 
+                Dui vivamus arcu felis bibendum. Nibh venenatis cras sed felis eget velit. Scelerisque in 
+                dictum non consectetur a. Interdum posuere lorem ipsum dolor. Aliquam sem et tortor 
+                consequat id. Eget felis eget nunc lobortis mattis. Orci eu lobortis elementum nibh 
+                tellus molestie. Gravida rutrum quisque non tellus orci ac auctor.
+            </p>
+            <p>
+                Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. 
+                Gravida arcu ac tortor dignissim. Cursus euismod quis viverra nibh cras pulvinar. 
+                Semper risus in hendrerit gravida. Tristique senectus et netus et 
+                malesuada fames. Venenatis urna cursus eget nunc scelerisque. Ultricies 
+                mi quis hendrerit dolor. Erat pellentesque adipiscing commodo elit at 
+                imperdiet dui. Bibendum arcu vitae elementum curabitur. Donec ac odio t
+                empor orci dapibus ultrices. Hac habitasse platea dictumst vestibulum 
+                rhoncus est pellentesque elit. Adipiscing elit pellentesque habitant morbi tristique. 
+                Elementum tempus egestas sed sed risus pretium. Mi in nulla posuere sollicitudin. 
+                Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. Nec ullamcorper 
+                sit amet risus nullam. Enim praesent elementum facilisis leo.
+            </p>
+            <p>
+                Cras adipiscing enim eu turpis egestas pretium. Ut enim blandit 
+                volutpat maecenas volutpat blandit aliquam etiam erat. Scelerisque 
+                viverra mauris in aliquam sem fringilla ut. Mi bibendum neque egestas 
+                congue quisque egestas diam in arcu. Mattis molestie a iaculis at erat 
+                pellentesque adipiscing commodo. Urna condimentum mattis pellentesque 
+                id nibh. Tellus at urna condimentum mattis pellentesque id. Vitae 
+                congue eu consequat ac felis. Sed velit dignissim sodales ut eu sem 
+                integer vitae. Non diam phasellus vestibulum lorem sed risus ultricies.
+            </p>
+        </div>
+    )
+}
