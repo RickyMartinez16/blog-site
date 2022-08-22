6377f7085c5a45803f4b2290cf7ccc60145fd152
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-22 18:31:09 -0400

    nav bar with links

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
+import NavBar from "./NavBar";
 
 function App() {
   return(
     <Router>
+      <Route>
+        <NavBar />
+      </Route>
       <Route exact={true} path="/">
          <HomePage /> 
       </Route>
