ceca92b464c43a1f6118c12e93494491f6938aeb
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-22 20:21:34 -0400

    link articles

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,13 +1,23 @@
 import React from "react"
 import {useParams} from "react-router-dom"
+import articleContent from "./article-content"
 
 function ArticlePage(){
     const {name} = useParams();
+    const article = articleContent.find(article => article.name === name)
+
+    if(!article){
+        return (
+            <h1>Article does not exist</h1>
+        )
+    }
     
     return (
         <div>
-            <h1>This is the {name} article</h1>
-            
+            <h1>This is the {article.title} article</h1>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
         </div>
     )
 }
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,9 +1,17 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import articleContent from "./article-content"
 
 function ArticlesListPage(){
     return (
         <div>
             <h1>Articles List</h1>
+            {articleContent.map((article, key) => (
+                <Link key={key} to={`article/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}...</p>
+                </Link>
+            ))}
             
         </div>
     )
